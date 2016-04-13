3ec59472cfc912ed4164049a18d225621195a238
keruihuang <keruihuang@gmail.com>
2016-04-12 22:28:33 -0500

    Create proxy.c

---
+++ proxy.c
@@ -0,0 +1,234 @@
+/*
+ * proxy.c - COMP 321 Web proxy
+ *
+ * TEAM MEMBERS:
+ *     John Q. Student, jqs@rice.edu 
+ *     Jane R. Student, jrs@rice.edu
+ */ 
+
+#include "csapp.h"
+
+/*
+ * Function prototypes
+ */
+static void	client_error(int fd, const char *cause, int err_num, 
+		    const char *short_msg, const char *long_msg);
+static char    *create_log_entry(const struct sockaddr_in *sockaddr,
+		    const char *uri, int size);
+static int	parse_uri(const char *uri, char **hostnamep, char **portp,
+		    char **pathnamep);
+
+/* 
+ * main
+ *
+ * Requires:
+ *   <to be filled in by the student(s)> 
+ *
+ * Effects:
+ *   <to be filled in by the student(s)> 
+ */
+int
+main(int argc, char **argv)
+{
+
+	/* Check the arguments. */
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
+		exit(0);
+	}
+
+	/* Return success. */
+	return (0);
+}
+
+/*
+ * parse_uri
+ * 
+ * Requires:
+ *   The parameter "uri" must point to a properly NUL-terminated string.
+ *
+ * Effects:
+ *   Given a URI from an HTTP proxy GET request (i.e., a URL), extract the
+ *   host name, port, and path name.  Create strings containing the host name,
+ *   port, and path name, and return them through the parameters "hostnamep",
+ *   "portp", "pathnamep", respectively.  (The caller must free the memory
+ *   storing these strings.)  Return -1 if there are any problems and 0
+ *   otherwise.
+ */
+static int
+parse_uri(const char *uri, char **hostnamep, char **portp, char **pathnamep)
+{
+	const char *pathname_begin, *port_begin, *port_end;
+
+	if (strncasecmp(uri, "http://", 7) != 0)
+		return (-1);
+
+	/* Extract the host name. */
+	const char *host_begin = uri + 7;
+	const char *host_end = strpbrk(host_begin, ":/ \r\n");
+	if (host_end == NULL)
+		host_end = host_begin + strlen(host_begin);
+	int len = host_end - host_begin;
+	char *hostname = Malloc(len + 1);
+	strncpy(hostname, host_begin, len);
+	hostname[len] = '\0';
+	*hostnamep = hostname;
+
+	/* Look for a port number.  If none is found, use port 80. */
+	if (*host_end == ':') {
+		port_begin = host_end + 1;
+		port_end = strpbrk(port_begin, "/ \r\n");
+		if (port_end == NULL)
+			port_end = port_begin + strlen(port_begin);
+		len = port_end - port_begin;
+	} else {
+		port_begin = "80";
+		port_end = host_end;
+		len = 2;
+	}
+	char *port = Malloc(len + 1);
+	strncpy(port, port_begin, len);
+	port[len] = '\0';
+	*portp = port;
+
+	/* Extract the path. */
+	if (*port_end == '/') {
+		pathname_begin = port_end;
+		const char *pathname_end = strpbrk(pathname_begin, " \r\n");
+		if (pathname_end == NULL)
+			pathname_end = pathname_begin + strlen(pathname_begin);
+		len = pathname_end - pathname_begin;
+	} else {
+		pathname_begin = "/";
+		len = 1;
+	}
+	char *pathname = Malloc(len + 1);
+	strncpy(pathname, pathname_begin, len);
+	pathname[len] = '\0';
+	*pathnamep = pathname;
+
+	return (0);
+}
+
+/*
+ * create_log_entry
+ *
+ * Requires:
+ *   The parameter "sockaddr" must point to a valid sockaddr_in structure.  The
+ *   parameter "uri" must point to a properly NUL-terminated string.
+ *
+ * Effects:
+ *   Returns a string containing a properly formatted log entry.  This log
+ *   entry is based upon the socket address of the requesting client
+ *   ("sockaddr"), the URI from the request ("uri"), and the size in bytes of
+ *   the response from the server ("size").
+ */
+static char *
+create_log_entry(const struct sockaddr_in *sockaddr, const char *uri, int size)
+{
+
+	/*
+	 * Create a large enough array of characters to store a log entry.
+	 * Although the length of the URI can exceed MAXLINE, the combined
+	 * lengths of the other fields and separators cannot.
+	 */
+	const size_t log_maxlen = MAXLINE + strlen(uri);
+	char *const log_str = Malloc(log_maxlen + 1);
+
+	/* Get a formatted time string. */
+	time_t now = time(NULL);
+	int log_strlen = strftime(log_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z: ",
+	    localtime(&now));
+
+	/*
+	 * Convert the IP address in network byte order to dotted decimal
+	 * form.
+	 */
+	Inet_ntop(AF_INET, &sockaddr->sin_addr, &log_str[log_strlen],
+	    INET_ADDRSTRLEN);
+	log_strlen += strlen(&log_str[log_strlen]);
+
+	/*
+	 * Assert that the time and IP address fields occupy less than half of
+	 * the space that is reserved for the non-URI fields.
+	 */
+	assert(log_strlen < MAXLINE / 2);
+
+	/*
+	 * Add the URI and response size onto the end of the log entry.
+	 */
+	snprintf(&log_str[log_strlen], log_maxlen - log_strlen, " %s %d", uri,
+	    size);
+
+	return (log_str);
+}
+
+/*
+ * client_error
+ *
+ * Requires:
+ *   The parameter "fd" must be an open socket that is connected to the client.
+ *   The parameters "cause", "short_msg", and "long_msg" must point to properly 
+ *   NUL-terminated strings that describe the reason why the HTTP transaction
+ *   failed.  The string "short_msg" may not exceed 32 characters in length,
+ *   and the string "long_msg" may not exceed 80 characters in length.
+ *
+ * Effects:
+ *   Constructs an HTML page describing the reason why the HTTP transaction
+ *   failed, and writes an HTTP/1.0 response containing that page as the
+ *   content.  The cause appearing in the HTML page is truncated if the
+ *   string "cause" exceeds 2048 characters in length.
+ */
+static void
+client_error(int fd, const char *cause, int err_num, const char *short_msg,
+    const char *long_msg)
+{
+	char body[MAXBUF], headers[MAXBUF], truncated_cause[2049];
+
+	assert(strlen(short_msg) <= 32);
+	assert(strlen(long_msg) <= 80);
+	/* Ensure that "body" is much larger than "truncated_cause". */
+	assert(sizeof(truncated_cause) < MAXBUF / 2);
+
+	/*
+	 * Create a truncated "cause" string so that the response body will not
+	 * exceed MAXBUF.
+	 */
+	strncpy(truncated_cause, cause, sizeof(truncated_cause) - 1);
+	truncated_cause[sizeof(truncated_cause) - 1] = '\0';
+
+	/* Build the HTTP response body. */
+	snprintf(body, MAXBUF,
+	    "<html><title>Proxy Error</title><body bgcolor=""ffffff"">\r\n"
+	    "%d: %s\r\n"
+	    "<p>%s: %s\r\n"
+	    "<hr><em>The COMP 321 Web proxy</em>\r\n",
+	    err_num, short_msg, long_msg, truncated_cause);
+
+	/* Build the HTTP response headers. */
+	snprintf(headers, MAXBUF,
+	    "HTTP/1.0 %d %s\r\n"
+	    "Content-type: text/html\r\n"
+	    "Content-length: %d\r\n"
+	    "\r\n",
+	    err_num, short_msg, (int)strlen(body));
+
+	/* Write the HTTP response. */
+	if (rio_writen(fd, headers, strlen(headers)) != -1)
+		rio_writen(fd, body, strlen(body));
+}
+
+/*
+ * The last lines of this file configure the behavior of the "Tab" key in
+ * emacs.  Emacs has a rudimentary understanding of C syntax and style.  In
+ * particular, depressing the "Tab" key once at the start of a new line will
+ * insert as many tabs and/or spaces as are needed for proper indentation.
+ */
+
+/* Local Variables: */
+/* mode: c */
+/* c-default-style: "bsd" */
+/* c-basic-offset: 8 */
+/* c-continued-statement-offset: 4 */
+/* indent-tabs-mode: t */
+/* End: */
